e43760a8ffc4286483f491b3df22e31d69198eb0
Sebastian Uribe <suribe@gmail.com>
2018-06-06 13:06:31 +0200

    - rename variables and made code slightly more readable

--- src/main/java/de/mfo/jsurf/rendering/cpu/Shader.java
+++ src/main/java/de/mfo/jsurf/rendering/cpu/Shader.java
@@ -8,7 +8,6 @@ import de.mfo.jsurf.rendering.LightProducts;
 import de.mfo.jsurf.rendering.LightSource;
 
 public class Shader {
-
     private final Color3f ambientColor;
 	private final LightSource[] lightSources;
 	private final LightProducts[] lightProducts;
@@ -23,14 +22,13 @@ public class Shader {
      * Shades a point with the same algorithm used by the
      * {@link <a href="http://surf.sourceforge.net">surf raytracer</a>}.
      * @param hitPoint Intersection point.
-     * @param v View vector (from intersection point to eye).
-     * @param n Surface normal.
-     * @param material Surface material.
+     * @param view View vector (from intersection point to eye).
+     * @param normal Surface normal.
      * @return
      */
-    protected Color3f shade( Point3d hitPoint, Vector3d v, Vector3d n)
+    protected Color3f shade( Point3d hitPoint, Vector3d view, Vector3d normal)
     {
-        Vector3d l = new Vector3d();
+        Vector3d lightDirection = new Vector3d();
         Vector3d h = new Vector3d();
 
         Color3f color = new Color3f( ambientColor );
@@ -38,21 +36,22 @@ public class Shader {
         for( int i = 0; i < lightSources.length; i++ )
         {
             LightSource lightSource = lightSources[i];
+            LightProducts lightProducts = this.lightProducts[i];
 
-            l.sub( lightSource.getPosition(), hitPoint );
-            l.normalize();
+            lightDirection.sub( lightSource.getPosition(), hitPoint );
+            lightDirection.normalize();
 
-            float lambertTerm = (float) n.dot( l );
+            float lambertTerm = (float) normal.dot( lightDirection );
             if( lambertTerm > 0.0f )
             {
                 // compute diffuse color component
-                color.scaleAdd( lambertTerm, lightProducts[i].getDiffuseProduct(), color );
+                color.scaleAdd( lambertTerm, lightProducts.getDiffuseProduct(), color );
 
                 // compute specular color component
-                h.add( l, v );
+                h.add( lightDirection, view );
                 h.normalize();
-
-                color.scaleAdd( ( float ) Math.pow( Math.max( 0.0f, n.dot( h ) ), lightProducts[i].getMaterial().getShininess() ), lightProducts[i].getSpecularProduct(), color );
+                double specularTerm = Math.pow( Math.max( 0.0f, normal.dot(h) ), lightProducts.getMaterial().getShininess() );
+              	color.scaleAdd( (float)specularTerm, lightProducts.getSpecularProduct(), color );
             }
         }
 
