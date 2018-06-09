35a6eec237e740af98efcac0c95a54b3d3a2e58d
Sebastian Uribe <suribe@gmail.com>
2018-06-09 13:12:02 +0200

    Move pixel render strategies to its own class

--- src/main/java/de/mfo/jsurf/rendering/cpu/RenderingTask.java
+++ src/main/java/de/mfo/jsurf/rendering/cpu/RenderingTask.java
@@ -121,6 +121,105 @@ public class RenderingTask implements Callable<Boolean>
     	}
     }
     
+    private abstract class PixelRenderStrategy {
+    	public abstract void renderPixel(int x, int y, PixelStep step, ColumnSubstitutorPair csp);
+    }
+    
+    private class AntiAliasedPixelRenderer extends PixelRenderStrategy {
+
+    	private final Color3f[] internalColorBuffer;
+    	private final float thresholdSqr;
+
+		public AntiAliasedPixelRenderer(DrawcallStaticData cdsd, Color3f[] internalColorBuffer) {
+			this.internalColorBuffer = internalColorBuffer;
+			this.thresholdSqr = dcsd.antiAliasingThreshold * dcsd.antiAliasingThreshold;
+		}
+    	
+		@Override
+		public void renderPixel(int x, int y, PixelStep step, ColumnSubstitutorPair csp) {
+			internalColorBuffer[ step.internalBufferIndex ] = tracePolynomial( csp.scs, csp.gcs, step.u, step.v );
+			if( x > 0 && y > 0 )
+			{
+			    Color3f urColor = internalColorBuffer[ step.internalBufferIndex ];
+			    Color3f ulColor = internalColorBuffer[ step.internalBufferIndex - 1 ];
+			    Color3f lrColor = internalColorBuffer[ step.internalBufferIndex - step.width ];
+			    Color3f llColor = internalColorBuffer[ step.internalBufferIndex - step.width - 1 ];
+
+			    dcsd.colorBuffer[ step.colorBufferIndex ] = antiAliasPixel( dcsd.antiAliasingPattern, ulColor, urColor, llColor, lrColor ).get().getRGB();
+			}
+		}
+
+	    private Color3f antiAliasPixel( AntiAliasingPattern aap, Color3f ulColor, Color3f urColor, Color3f llColor, Color3f lrColor )
+	    {
+	        // first average pixel-corner colors
+	        Color3f finalColor;
+
+	        // adaptive supersampling
+	        if( aap != AntiAliasingPattern.OG_2x2 && ( colorDiffSqr( ulColor, urColor ) >= thresholdSqr ||
+	            colorDiffSqr( ulColor, llColor ) >= thresholdSqr ||
+	            colorDiffSqr( ulColor, lrColor ) >= thresholdSqr ||
+	            colorDiffSqr( urColor, llColor ) >= thresholdSqr ||
+	            colorDiffSqr( urColor, lrColor ) >= thresholdSqr ||
+	            colorDiffSqr( llColor, lrColor ) >= thresholdSqr ) )
+	        {
+	            // anti-alias pixel with advanced sampling pattern
+	            finalColor = new Color3f();
+	            for( AntiAliasingPattern.SamplingPoint sp : aap )
+	            {
+	                if( Thread.currentThread().isInterrupted() )
+	                    throw new RenderingInterruptedException();
+
+	                Color3f ss_color;
+	                if( sp.getU() == 0.0 && sp.getV() == 0.0 )
+	                    ss_color = llColor;
+	                else if( sp.getU() == 0.0 && sp.getV() == 1.0 )
+	                    ss_color = ulColor;
+	                else if( sp.getU() == 1.0 && sp.getV() == 1.0 )
+	                    ss_color = urColor;
+	                else if( sp.getU() == 1.0 && sp.getV() == 0.0 )
+	                    ss_color = lrColor;
+	                else
+	                {
+	                    // color of this sample point is not known -> calculate
+	                    double v = step.vOld + sp.getV() * step.v_incr;
+	                    double u = step.uOld + sp.getU() * step.u_incr;
+	                    ColumnSubstitutorPair csp = cspProvider.get( v );
+	                    ss_color = tracePolynomial( csp.scs, csp.gcs, u, v );
+	                }
+	                finalColor.scaleAdd( sp.getWeight(), ss_color, finalColor );
+	                
+	                if( false )
+	                    return new Color3f( 0, 0, 0 ); // paint pixels, that are supposed to be anti-aliased in black
+	            }
+	        }
+	        else
+	        {
+	            finalColor = new Color3f( ulColor );
+	            finalColor.add( urColor );
+	            finalColor.add( llColor );
+	            finalColor.add( lrColor );
+	            finalColor.scale( 0.25f );
+	        }
+
+	        // clamp color, because floating point operations may yield values outside [0,1]
+	        finalColor.clamp( 0f, 1f );
+	        return finalColor;
+	    }
+
+	    private float colorDiffSqr( Color3f c1, Color3f c2 )
+	    {
+	        Vector3f diff = new Vector3f( c1 );
+	        diff.sub( c2 );
+	        return diff.dot( diff );
+	    }
+    }
+
+    private class BasicPixelRenderer extends PixelRenderStrategy {
+		@Override public void renderPixel(int x, int y, PixelStep step, ColumnSubstitutorPair csp) {
+			dcsd.colorBuffer[ step.colorBufferIndex ] = tracePolynomial(csp.scs, csp.gcs, step.u, step.v ).get().getRGB();
+		}
+    }
+    
 	static ColorBufferPool bufferPool = new ColorBufferPool();
 	
     // initialized by the constructor
@@ -142,12 +241,14 @@ public class RenderingTask implements Callable<Boolean>
     public Boolean call() {
 		Color3f[] colorBuffer = null;
         try {
+        	PixelRenderStrategy pixelRenderer; 
         	if (useAntiAliasing()) {
         		colorBuffer = bufferPool.getBuffer(step.width * step.height);
-        		renderWithAntiAliasing(colorBuffer, step);
+        		pixelRenderer = new AntiAliasedPixelRenderer(dcsd, colorBuffer);
         	} else {
-        		renderWithoutAntiAliasing(step);
+        		pixelRenderer = new BasicPixelRenderer();
         	}
+        	renderImage(pixelRenderer);
             return true;
         } catch( RenderingInterruptedException rie ) { // rendering interrupted .. that's ok
         } catch( Throwable t ) {
@@ -163,7 +264,7 @@ public class RenderingTask implements Callable<Boolean>
     	return dcsd.antiAliasingPattern != AntiAliasingPattern.OG_1x1;
     }
 
-	private void renderWithAntiAliasing(Color3f[] internalColorBuffer, PixelStep step) {
+	private void renderImage(PixelRenderStrategy pixelRenderer) {
 		for( int y = 0; y < step.height; ++y )
 		{
 			ColumnSubstitutorPair csp = cspProvider.get(step.v);
@@ -173,107 +274,12 @@ public class RenderingTask implements Callable<Boolean>
 		        if( Thread.currentThread().isInterrupted() )
 		            throw new RenderingInterruptedException();
 		        
-		        renderPixelWithAntiAliasing(x, y, step, csp, internalColorBuffer);
+		        pixelRenderer.renderPixel(x, y, step, csp);
 		        step.stepU();
 		    }
 		    step.stepV();
 		}
 	}
-
-	/** no antialising -> sample pixel center */
-	private void renderWithoutAntiAliasing(PixelStep step) {
-
-		for( int y = 0; y < step.height; y++ )
-		{
-			ColumnSubstitutorPair csp = cspProvider.get(step.v);
-			
-		    for( int x = 0; x < step.width; x++ )
-		    {
-		        if( Thread.currentThread().isInterrupted() )
-		            throw new RenderingInterruptedException();
-
-		        renderPixelWithoutAntiAliasing(step, csp);
-			    step.stepU();
-		    }
-			step.stepV();
-		}
-	}
-
-	private void renderPixelWithAntiAliasing(int x, int y, PixelStep step, ColumnSubstitutorPair csp, Color3f[] internalColorBuffer) {
-		internalColorBuffer[ step.internalBufferIndex ] = tracePolynomial( csp.scs, csp.gcs, step.u, step.v );
-		if( x > 0 && y > 0 )
-		{
-		    Color3f urColor = internalColorBuffer[ step.internalBufferIndex ];
-		    Color3f ulColor = internalColorBuffer[ step.internalBufferIndex - 1 ];
-		    Color3f lrColor = internalColorBuffer[ step.internalBufferIndex - step.width ];
-		    Color3f llColor = internalColorBuffer[ step.internalBufferIndex - step.width - 1 ];
-
-		    dcsd.colorBuffer[ step.colorBufferIndex ] = antiAliasPixel( dcsd.antiAliasingPattern, ulColor, urColor, llColor, lrColor ).get().getRGB();
-		}
-	}
-
-
-	private void renderPixelWithoutAntiAliasing(PixelStep step, ColumnSubstitutorPair csp) {
-		dcsd.colorBuffer[ step.colorBufferIndex ] = tracePolynomial(csp.scs, csp.gcs, step.u, step.v ).get().getRGB();
-	}
-	
-    private Color3f antiAliasPixel( AntiAliasingPattern aap, Color3f ulColor, Color3f urColor, Color3f llColor, Color3f lrColor )
-    {
-        // first average pixel-corner colors
-        Color3f finalColor;
-
-        // adaptive supersampling
-        float thresholdSqr = dcsd.antiAliasingThreshold * dcsd.antiAliasingThreshold;
-        if( aap != AntiAliasingPattern.OG_2x2 && ( colorDiffSqr( ulColor, urColor ) >= thresholdSqr ||
-            colorDiffSqr( ulColor, llColor ) >= thresholdSqr ||
-            colorDiffSqr( ulColor, lrColor ) >= thresholdSqr ||
-            colorDiffSqr( urColor, llColor ) >= thresholdSqr ||
-            colorDiffSqr( urColor, lrColor ) >= thresholdSqr ||
-            colorDiffSqr( llColor, lrColor ) >= thresholdSqr ) )
-        {
-            // anti-alias pixel with advanced sampling pattern
-            finalColor = new Color3f();
-            for( AntiAliasingPattern.SamplingPoint sp : aap )
-            {
-                if( Thread.currentThread().isInterrupted() )
-                    throw new RenderingInterruptedException();
-
-                Color3f ss_color;
-                if( sp.getU() == 0.0 && sp.getV() == 0.0 )
-                    ss_color = llColor;
-                else if( sp.getU() == 0.0 && sp.getV() == 1.0 )
-                    ss_color = ulColor;
-                else if( sp.getU() == 1.0 && sp.getV() == 1.0 )
-                    ss_color = urColor;
-                else if( sp.getU() == 1.0 && sp.getV() == 0.0 )
-                    ss_color = lrColor;
-                else
-                {
-                    // color of this sample point is not known -> calculate
-                    double v = step.vOld + sp.getV() * step.v_incr;
-                    double u = step.uOld + sp.getU() * step.u_incr;
-                    ColumnSubstitutorPair csp = cspProvider.get( v );
-                    ss_color = tracePolynomial( csp.scs, csp.gcs, u, v );
-                }
-                finalColor.scaleAdd( sp.getWeight(), ss_color, finalColor );
-                
-                if( false )
-                    return new Color3f( 0, 0, 0 ); // paint pixels, that are supposed to be anti-aliased in black
-            }
-        }
-        else
-        {
-            finalColor = new Color3f( ulColor );
-            finalColor.add( urColor );
-            finalColor.add( llColor );
-            finalColor.add( lrColor );
-            finalColor.scale( 0.25f );
-        }
-
-        // clamp color, because floating point operations may yield values outside [0,1]
-        finalColor.clamp( 0f, 1f );
-        return finalColor;
-    }
 
     private Color3f tracePolynomial( ColumnSubstitutor scs, ColumnSubstitutorForGradient gcs, double u, double v )
     {
@@ -381,13 +387,6 @@ public class RenderingTask implements Callable<Boolean>
 //        return dcsd.backgroundColor;
 //    }
 
-    private float colorDiffSqr( Color3f c1, Color3f c2 )
-    {
-        Vector3f diff = new Vector3f( c1 );
-        diff.sub( c2 );
-        return diff.dot( diff );
-    }
-
     protected boolean intersectPolynomial( UnivariatePolynomial p, double rayStart, double rayEnd, double[] hit )
     {   
         //System.out.println( p );
@@ -426,5 +425,3 @@ public class RenderingTask implements Callable<Boolean>
     }
 
 }
-    
-
