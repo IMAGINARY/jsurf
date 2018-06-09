2e8b2f832935fd37841917efa7c73d41c86d8937
Sebastian Uribe <suribe@gmail.com>
2018-06-09 16:16:02 +0200

    Move all corner point related calculation outside of the super sampling function

--- src/main/java/de/mfo/jsurf/rendering/cpu/AntiAliasedPixelRenderer.java
+++ src/main/java/de/mfo/jsurf/rendering/cpu/AntiAliasedPixelRenderer.java
@@ -21,19 +21,14 @@ class AntiAliasedPixelRenderer extends PixelRenderStrategy {
 	@Override
 	public void renderPixel(int x, int y, PixelStep step, ColumnSubstitutorPair csp) {
 		internalColorBuffer[ step.internalBufferIndex ] = tracePolynomial( csp.scs, csp.gcs, step.u, step.v );
-		if( x > 0 && y > 0 )
-		{
-		    Color3f urColor = internalColorBuffer[ step.internalBufferIndex ];
-		    Color3f ulColor = internalColorBuffer[ step.internalBufferIndex - 1 ];
-		    Color3f lrColor = internalColorBuffer[ step.internalBufferIndex - step.width ];
-		    Color3f llColor = internalColorBuffer[ step.internalBufferIndex - step.width - 1 ];
+		if( x == 0 || y == 0 )
+			return;
+		
+	    Color3f urColor = internalColorBuffer[ step.internalBufferIndex ];
+	    Color3f ulColor = internalColorBuffer[ step.internalBufferIndex - 1 ];
+	    Color3f lrColor = internalColorBuffer[ step.internalBufferIndex - step.width ];
+	    Color3f llColor = internalColorBuffer[ step.internalBufferIndex - step.width - 1 ];
 
-		    colorBuffer[ step.colorBufferIndex ] = antiAliasPixel( step, ulColor, urColor, llColor, lrColor ).get().getRGB();
-		}
-	}
-
-    private Color3f antiAliasPixel( PixelStep step, Color3f ulColor, Color3f urColor, Color3f llColor, Color3f lrColor )
-    {
         boolean doSuperSampling = 
         	aap != AntiAliasingPattern.OG_2x2 &&
         	(thresholdSqr == 0 ||
@@ -50,29 +45,32 @@ class AntiAliasedPixelRenderer extends PixelRenderStrategy {
         accumulator.add( llColor );
         accumulator.add( lrColor );
         accumulator.scale( doSuperSampling ? aap.cornerWeight : 0.25f);
-
+        
         if (doSuperSampling)
-        {
-            for( AntiAliasingPattern.SamplingPoint sp : aap )
-            {
-                if( Thread.currentThread().isInterrupted() )
-                    throw new RenderingInterruptedException();
+        	sampleExceptCorners(step, accumulator);
 
-                // corners already accumulated above
-                if (sp.isCorner)
-                	continue;
-                
-                double v = step.vOld + sp.v * step.v_incr;
-                double u = step.uOld + sp.u * step.u_incr;
-                ColumnSubstitutorPair csp = cspProvider.get( v );
-                Color3f sample = tracePolynomial( csp.scs, csp.gcs, u, v );
-                accumulator.scaleAdd( sp.weight, sample, accumulator );
-            }
-        }
-
-        // clamp color, because floating point operations may yield values outside [0,1]
         accumulator.clamp( 0f, 1f );
-        return accumulator;
+
+	    colorBuffer[ step.colorBufferIndex ] = accumulator.get().getRGB();
+	}
+
+    private void sampleExceptCorners( PixelStep step, Color3f accumulator )
+    {
+        for( AntiAliasingPattern.SamplingPoint sp : aap )
+        {
+            if( Thread.currentThread().isInterrupted() )
+                throw new RenderingInterruptedException();
+
+            // corners already accumulated above
+            if (sp.isCorner)
+            	continue;
+            
+            double v = step.vOld + sp.v * step.v_incr;
+            double u = step.uOld + sp.u * step.u_incr;
+            ColumnSubstitutorPair csp = cspProvider.get( v );
+            Color3f sample = tracePolynomial( csp.scs, csp.gcs, u, v );
+            accumulator.scaleAdd( sp.weight, sample, accumulator );
+        }
     }
     
     private boolean areTooDifferent(Color3f c1, Color3f c2) {
